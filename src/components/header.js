a4534339547f998180c07545a99ac188ee60806d
vkngr <96548883+vkngr@users.noreply.github.com>
2022-01-09 17:13:37 +0000

    Hero Ready V1

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,43 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Header = ({ siteTitle }) => (
-  <header className="bg-stone-200 mb-4">
-    <div className="max-w-screen-lg p-6">
-      <h1>
-        <Link to="/" className="text-stone-700">
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="flex justify-center">
+    <div className="w-full max-w-screen-lg p-6 flex flex-row items-center">
+      {/* Brand */}
+      <Link to="/" className="text-stone-700">
+        <StaticImage
+          src="../images/black-mountain.svg"
+          alt="A mountain graphic"
+          className="mr-3"
+        />
+        <span className=" text-xl">Fjallaþjónustan</span>
+      </Link>
+
+      {/* Navigation */}
+      <div className="ml-auto">
+        <ul className="hidden md:flex gap-6">
+          <li className="text-lg">Experiences</li>
+          <li className="text-lg">About us</li>
+          <li className="text-lg">Get in touch</li>
+        </ul>
+        <a className="md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,77 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Header from "../components/header"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1 className="text-3xl font-bold text-sky-700">Hello world!</h1>
-  </Layout>
-)
+import { StaticImage } from "gatsby-plugin-image"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <Header />
+      <div className="flex justify-center">
+        <div className="max-w-screen-lg p-6">
+          <main>
+            {/* Hero Heading */}
+            <h1 className="text-3xl md:text-5xl text-stone-700 mt-24">
+              Come and explore the incredible Iceland with your local expert
+              guide
+            </h1>
+
+            {/* Hero Navigation */}
+            <ul className="mt-12 flex flex-col md:flex-row gap-6">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-sky-700"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <span className="text-lg">Experiences</span>
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-sky-700"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <span className="text-lg">Check out our blog</span>
+              </li>
+            </ul>
+          </main>
+        </div>
+      </div>
+
+      {/* Hero Image */}
+      <div className="relative">
+        <StaticImage
+          src="../images/basar-hut-1.jpg"
+          alt="A mountain graphic"
+          className="min-h-[420px] mt-24"
+        />
+        <div className="absolute bottom-6 right-6 z-10">
+          <span className="text-white text-lg">
+            Básar Hut & Campsite, Þórsmörk
+          </span>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
