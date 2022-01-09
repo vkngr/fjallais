703fbfbef7dc7644a19fd96ca91f56332fa41024
vkngr <96548883+vkngr@users.noreply.github.com>
2022-01-09 21:48:31 +0000

    Experiences now fetch and work

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -3,11 +3,26 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Experience = () => (
-  <Layout>
-    <Seo title="Experience" />
-    <h1>Experience page</h1>
-  </Layout>
-)
+import JSONData from "../data/experiences.json"
+
+const Experience = ({ id }) => {
+  let search = JSONData.filter(item => item.slug == id)
+
+  if (search.length <= 0) {
+    return (
+      <Layout>
+        <Seo title="Experience" />
+        EXPERIENCE NOT FOUND
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <Seo title="Experience" />
+        {search[0].displayName}
+      </Layout>
+    )
+  }
+}
 
 export default Experience
