f4da0b6828669a263f9d257ec340aae49baf0b68
vkngr <96548883+vkngr@users.noreply.github.com>
2022-01-12 22:32:00 +0000

    Update booking.js

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -196,40 +196,44 @@ export default class BookingPage extends React.Component {
                 <p className="text-xl font-medium mt-6">Booking status</p>
 
                 {bookingStatusMessages.map((e, i) => (
-                  <p className="text-xl font-medium mt-6 ">
-                    <div className="flex gap-3 items-center">
-                      {this.state.bookingStatus < i + 1 ? (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                        </svg>
-                      ) : (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      )}
-                      <span>{e}</span>
-                    </div>
-                  </p>
+                  <ul>
+                    <li key={i}>
+                      <p className="text-xl font-medium mt-6 ">
+                        <div className="flex gap-3 items-center">
+                          {this.state.bookingStatus < i + 1 ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-6 w-6"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              />
+                            </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-6 w-6"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          )}
+                          <span>{e}</span>
+                        </div>
+                      </p>
+                    </li>
+                  </ul>
                 ))}
               </div>
             </div>
